53225668ee76dcf6906d3065008905ac11a6ca9d
a-poor <45295232+a-poor@users.noreply.github.com>
2021-05-28 22:18:26 -0700

    Adding all-list table

--- site/src/AllListsPage/AllListsPage.js
+++ site/src/AllListsPage/AllListsPage.js
@@ -1,4 +1,4 @@
-import { Layout, Menu, Breadcrumb, List, Checkbox, Typography, Space, Tooltip, Row, Col, Avatar, Table } from 'antd';
+import { Layout, Menu, Button, List, Checkbox, Typography, Space, Tooltip, Row, Col, Avatar, Table } from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined, FormOutlined, SettingOutlined } from '@ant-design/icons';
 
 const { SubMenu } = Menu;
@@ -6,26 +6,55 @@ const { Header, Content, Sider } = Layout;
 const { Text } = Typography;
 
 const dataSource = [
-    {name: "Groceries", users: ["Me", "Lauren"]},
-    {name: "Wish List", users: ["Me"]},
-    {name: "Weekend Plans", users: ["Me", "Lauren"]},
-    {name: "Blog Ideas", users: ["Me"]}
+    {id: "abc123", name: "Groceries", owner: "Me", users: ["Me", "Lauren"]},
+    {id: "a1b2c3", name: "Wish List", owner: "Me", users: ["Me"]},
+    {id: "c32a1b", name: "Weekend Plans", owner: "Lauren", users: ["Me", "Lauren", "Sandwich"]},
+    {id: "b2ac13", name: "Blog Ideas", owner: "Me", users: ["Me"]}
+];
+
+const columns = [
+    {
+        title: "ToDoers",
+        dataIndex: "users",
+        render: users => (
+            <Avatar.Group>
+                { users.map((u,i) => <Avatar key={i}>{ u[0].toUpperCase() }</Avatar> )}
+            </Avatar.Group>
+        )
+    },
+    {
+        title: "List Name",
+        dataIndex: "name"
+    },
+    {
+        title: "List Owner",
+        dataIndex: "owner",
+        render: u => (
+            <Space>
+                <Avatar>{ u[0].toUpperCase() }</Avatar>
+                <Text>{ u }</Text>
+            </Space>
+        ) 
+    },
+    {
+        title: "Actions",
+        dataIndex: "id",
+        render: id => (
+            <Space>
+                <Button type="primary" shape="round">Open</Button>
+                <Button type="secondary" shape="round">Share</Button>
+                <Button danger type="primary" shape="round">Delete</Button>
+            </Space>
+        )
+    },
 ];
 
 
 function AllListsPage() {
-    return <List 
+    return <Table 
         dataSource={ dataSource }
-        renderItem={d => (
-            <List.Item>
-                { d.name }
-            </List.Item>
-        )}
+        columns={columns}
     />;
-}
-
-function ListItem() {
-    return <> </>;
 }
 
 export default AllListsPage;
